1e3dfe53a2531ee881f6b0184670ae8154cc8184
HAERIN-L <114037790+HAERIN-L@users.noreply.github.com>
2024-03-09 18:36:26 +0900

    Update 단어 뒤집기.py
    주석 추가

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(n):
-  s = list(input().split())
+  s = list(input().split()) # 공백 기준으로 단어 분리 후, 리스트로 저장
 
   for j in s:
-    print(j[::-1], end=' ')
+    print(j[::-1], end=' ') # 단어 뒤집어서 출력하기
