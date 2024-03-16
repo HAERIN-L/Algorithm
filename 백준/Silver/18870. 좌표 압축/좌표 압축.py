fc95b2565ac8bc550f995930835828a1fa28f9b5
HAERIN-L <114037790+HAERIN-L@users.noreply.github.com>
2024-03-16 15:39:03 +0900

    [Silver II] Title: 좌표 압축, Time: 1628 ms, Memory: 180240 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,17 @@
+def compress_coordinates(coords):
+    # 정렬된 좌표와 원래 좌표의 매핑을 저장할 딕셔너리
+    rank = {v: i for i, v in enumerate(sorted(set(coords)))}
+    # 압축된 좌표를 저장할 리스트
+    compressed = [rank[x] for x in coords]
+    return compressed
+
+# 입력 받기
+n = int(input())
+coords = list(map(int, input().split()))
+
+# 좌표 압축 실행
+compressed_coords = compress_coordinates(coords)
+
+# 결과 출력
+print(' '.join(map(str, compressed_coords)))
+
