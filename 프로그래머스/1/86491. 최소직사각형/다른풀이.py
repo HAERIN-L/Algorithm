3167a9cb12a340a21cc1dcc6eea9e7c1eda76434
HAERIN-L <114037790+HAERIN-L@users.noreply.github.com>
2025-09-04 17:23:39 +0900

    new

--- 프로그래머스/1/86491. 최소직사각형/다른풀이.py
+++ 프로그래머스/1/86491. 최소직사각형/다른풀이.py
@@ -1,10 +1,2 @@
 def solution(sizes):
-    w=[]
-    h=[]
-    for x, y in sizes:
-        big = max(x,y)
-        small = min(x,y)
-        w.append(big)
-        h.append(small)
-        
-    return max(w)*max(h)
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
