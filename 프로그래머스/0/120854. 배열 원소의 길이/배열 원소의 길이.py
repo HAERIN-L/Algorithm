636fc320e371278bfa4ded5c0a81af37d935cb8c
HAERIN-L <114037790+HAERIN-L@users.noreply.github.com>
2026-01-17 16:58:04 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -0,0 +1,8 @@
+# def solution(strlist):
+#     answer =[]
+#     for i in strlist:
+#         answer.append(len(i))
+#     return answer
+
+def solution(strlist):
+    return [len(i) for i in strlist]
